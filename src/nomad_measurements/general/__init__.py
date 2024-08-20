cf8477af18aef41b5f8808e7743d1808601011f5
Andrea Albino <95371554+aalbino2@users.noreply.github.com>
2024-08-20 12:51:52 +0200

    Apply suggestions from Sarthak's code review
    Co-authored-by: Sarthak Kapoor <57119427+ka-sarthak@users.noreply.github.com>

--- src/nomad_measurements/general/__init__.py
+++ src/nomad_measurements/general/__init__.py
@@ -9,7 +9,7 @@ class GeneralSchemaPackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
-general_schema = GeneralSchemaPackageEntryPoint(
+schema = GeneralSchemaPackageEntryPoint(
     name='GeneralSchema',
     description='Schema package defined using the new plugin mechanism.',
 )
