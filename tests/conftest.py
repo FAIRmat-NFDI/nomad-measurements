8caad2865c506fe3b1d4b73d0c18f6b0269354d9
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-08-28 15:29:10 +0200

    Add fixture to capture error from logs

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from nomad.utils import structlogging
+import json
+
+
+@pytest.fixture(scope='function')
+def capture_error_from_logger(caplog):
+    """
+    Extracts log messages from the logger and raises an assertion error if any
+    ERROR messages are found.
+    """
+    caplog.handler.formatter = structlogging.ConsoleFormatter()
+    yield caplog
+    for record in caplog.get_records(when='call'):
+        if record.levelname in ['ERROR']:
+            try:
+                msg = structlogging.ConsoleFormatter.serialize(json.loads(record.msg))
+            except Exception:
+                msg = record.msg
+            assert False, msg
