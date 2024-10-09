7432c3f39986b69ce12399e91cc7cac510b5bf5f
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-10-09 12:04:07 +0200

    Abstracting out entry data def as a attr of the parser

--- src/nomad_measurements/xrd/parser.py
+++ src/nomad_measurements/xrd/parser.py
@@ -35,12 +35,15 @@ class XRDParser(MatchingParser):
     """
     Parser for matching XRD files and creating instances of ELNXRayDiffraction
     """
+    def set_entrydata_definition(self):
+        self.entrydata_definition = ELNXRayDiffraction
 
     def parse(
         self, mainfile: str, archive: 'EntryArchive', logger=None, child_archives=None
     ) -> None:
+        self.set_entrydata_definition()
         data_file = mainfile.split('/')[-1]
-        entry = ELNXRayDiffraction.m_from_dict(ELNXRayDiffraction.m_def.a_template)
+        entry = self.entrydata_definition.m_from_dict(self.entrydata_definition.m_def.a_template)
         entry.data_file = data_file
         file_name = f'{"".join(data_file.split(".")[:-1])}.archive.json'
         archive.data = RawFileXRDData(
