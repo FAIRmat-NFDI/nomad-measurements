82555303746e945aaeca87bd49df070e7f2fa534
Sarthak Kapoor <57119427+ka-sarthak@users.noreply.github.com>
2024-11-19 17:24:31 +0100

    114 looks for errors in the structlogger in tests (#115)
    * Add fixture to capture error from logs
    
    * Add nomad-lab infrastructure deps required by caplog fixture
    
    * Ruff
    
    * Refactoring fixtures; parameterize caplog to allow capturing different log levels
    
    * Removing the formatter from nomad.utils
    
    * testing: add logger.error in normalize
    
    * testing: adding the formatter from nomad.utils
    
    * Add logstash dep; cleaning
    
    * Cleaning
    
    * Specify caplog as an arg (from review)

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import logging
+
+import pytest
+import structlog
+from nomad.utils import structlogging
+from structlog.testing import LogCapture
+
+structlogging.ConsoleFormatter.short_format = True
+setattr(logging, 'Formatter', structlogging.ConsoleFormatter)
+
+
+@pytest.fixture(
+    name='caplog',
+    scope='function',
+)
+def fixture_caplog(request):
+    """
+    Extracts log messages from the logger and raises an assertion error if the specified
+    log levels in the `request.param` are found.
+    """
+    caplog = LogCapture()
+    processors = structlog.get_config()['processors']
+    old_processors = processors.copy()
+
+    try:
+        processors.clear()
+        processors.append(caplog)
+        structlog.configure(processors=processors)
+        yield caplog
+        for record in caplog.entries:
+            if record['log_level'] in request.param:
+                assert False, record
+    finally:
+        processors.clear()
+        processors.extend(old_processors)
+        structlog.configure(processors=processors)
