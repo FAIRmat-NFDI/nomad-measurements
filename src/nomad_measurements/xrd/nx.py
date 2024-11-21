005c5f5ec233c4797023a542d5699f03a5aaf5fc
Rubel <rubel.mozumder@outlook.com>
2024-11-21 14:58:54 +0100

    Reference a nxs file into a quantity, still it needs to be discussed where this reference should go.

--- src/nomad_measurements/xrd/nx.py
+++ src/nomad_measurements/xrd/nx.py
@@ -15,13 +15,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from importlib import metadata
 from typing import TYPE_CHECKING
 
 import os
 
-from pynxtools.nomad.parser import NexusParser
-from nomad.datamodel.datamodel import EntryArchive, EntryMetadata
+# from pynxtools.nomad.parser import NexusParser
+# from nomad.datamodel.datamodel import EntryArchive, EntryMetadata
 from pynxtools.dataconverter import writer as pynxtools_writer
 from pynxtools.dataconverter.helpers import (
     get_nxdl_root_and_path,
@@ -35,98 +34,6 @@ if TYPE_CHECKING:
     from structlog.stdlib import (
         BoundLogger,
     )
-
-
-# import json
-# import math
-# import os
-
-# import numpy as np
-# import pandas as pd
-# import yaml
-# from epic_scraper.epicfileimport.epic_module import (
-#     epiclog_read,
-# )
-# from nomad.datamodel.context import ClientContext
-# from nomad.datamodel.metainfo.basesections import (
-#     ExperimentStep,
-# )
-# from nomad.units import ureg
-
-
-# def get_entry_id(upload_id, filename):
-#     from nomad.utils import hash
-
-#     return hash(upload_id, filename)
-
-
-# def get_hash_ref(upload_id, filename):
-#     return f'{get_reference(upload_id, get_entry_id(upload_id, filename))}#data'
-
-
-# def nan_equal(a, b):
-#     """
-#     Compare two values with NaN values.
-#     """
-#     if isinstance(a, float) and isinstance(b, float):
-#         return a == b or (math.isnan(a) and math.isnan(b))
-#     elif isinstance(a, dict) and isinstance(b, dict):
-#         return dict_nan_equal(a, b)
-#     elif isinstance(a, list) and isinstance(b, list):
-#         return list_nan_equal(a, b)
-#     else:
-#         return a == b
-
-
-# def list_nan_equal(list1, list2):
-#     """
-#     Compare two lists with NaN values.
-#     """
-#     if len(list1) != len(list2):
-#         return False
-#     for a, b in zip(list1, list2):
-#         if not nan_equal(a, b):
-#             return False
-#     return True
-
-
-# def dict_nan_equal(dict1, dict2):
-#     """
-#     Compare two dictionaries with NaN values.
-#     """
-#     if set(dict1.keys()) != set(dict2.keys()):
-#         return False
-#     for key in dict1:
-#         if not nan_equal(dict1[key], dict2[key]):
-#             return False
-#     return True
-
-
-# def create_archive(
-#     entry_dict, context, filename, file_type, logger, *, overwrite: bool = False
-# ):
-#     file_exists = context.raw_path_exists(filename)
-#     dicts_ae_equal = None
-#     if isinstance(context, ClientContext):
-#         return None
-#     if file_exists:
-#         with context.raw_file(filename, 'r') as file:
-#             existing_dict = yaml.safe_load(file)
-#             dicts_are_equal = dict_nan_equal(existing_dict, entry_dict)
-#     if not file_exists or overwrite or dicts_are_equal:
-#         with context.raw_file(filename, 'w') as newfile:
-#             if file_type == 'json':
-#                 json.dump(entry_dict, newfile)
-#             elif file_type == 'yaml':
-#                 yaml.dump(entry_dict, newfile)
-#         context.upload.process_updated_raw_file(filename, allow_modify=True)
-#     elif file_exists and not overwrite and not dicts_are_equal:
-#         logger.error(
-#             f'{filename} archive file already exists. '
-#             f'You are trying to overwrite it with a different content. '
-#             f'To do so, remove the existing archive and click reprocess again.'
-#         )
-#     return get_hash_ref(context.upload_id, filename)
 
 
 def connect_concepts_from_dict(xrd_dict, template, scan_type: str):
