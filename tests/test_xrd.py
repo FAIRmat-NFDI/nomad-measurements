91afe96e657a422a75f3ce9b7b5d9f54e261ca64
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-05-23 15:37:28 +0200

    Check for both 1D result sections

--- tests/test_xrd.py
+++ tests/test_xrd.py
@@ -20,7 +20,7 @@ import os
 import pytest
 from nomad.client import normalize_all
 
-from nomad_measurements.xrd.schema import XRDResult1DHDF5
+from nomad_measurements.xrd.schema import XRDResult1D, XRDResult1DHDF5
 
 test_files = [
     'tests/data/xrd/XRD-918-16_10.xrdml',
@@ -58,7 +58,9 @@ def test_normalize_all(parsed_measurement_archive, caplog):
     assert parsed_measurement_archive.data.results[
         0
     ].source_peak_wavelength.magnitude == pytest.approx(1.540598, 1e-2)
-    if isinstance(parsed_measurement_archive.data.results[0], XRDResult1DHDF5):
+    if isinstance(
+        parsed_measurement_archive.data.results[0], XRDResult1D | XRDResult1DHDF5
+    ):
         assert (
             parsed_measurement_archive.results.properties.structural.diffraction_pattern[
                 0
