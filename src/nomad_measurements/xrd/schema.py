cd1b3975c568a109852f8686264f0d711eb66737
Hampus Näsström <hampus.nasstrom@gmail.com>
2025-07-08 11:56:58 +0200

    Changed ParserSection import

--- src/nomad_measurements/xrd/schema.py
+++ src/nomad_measurements/xrd/schema.py
@@ -51,7 +51,7 @@ from nomad.datamodel.metainfo.basesections import (
     MeasurementResult,
     ReadableIdentifiers,
 )
-from nomad.datamodel.metainfo.basesections.v1 import ElnParserSection
+from nomad.datamodel.metainfo.eln import ElnParserSection
 from nomad.datamodel.metainfo.plot import PlotlyFigure, PlotSection
 from nomad.datamodel.results import (
     DiffractionPattern,
