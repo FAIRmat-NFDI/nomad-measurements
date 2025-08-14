60fb78c259b46913cf398a8da483ea426f67a4af
Hampus Näsström <hampus.nasstrom@gmail.com>
2025-08-14 16:19:58 +0200

    Changed `utils.create_archive` to use `update_entry`

--- src/nomad_measurements/utils.py
+++ src/nomad_measurements/utils.py
@@ -69,20 +69,15 @@ def create_archive(
     entity: 'ArchiveSection',
     archive: 'EntryArchive',
     file_name: str,
+    overwrite: bool=False,
 ) -> str:
-    import json
-
-    from nomad.datamodel.context import ClientContext
-
-    entity_entry = entity.m_to_dict(with_root_def=True)
-    if isinstance(archive.m_context, ClientContext):
-        with open(file_name, 'w') as outfile:
-            json.dump({'data': entity_entry}, outfile, indent=4)
-        return os.path.abspath(file_name)
-    if not archive.m_context.raw_path_exists(file_name):
-        with archive.m_context.raw_file(file_name, 'w') as outfile:
-            json.dump({'data': entity_entry}, outfile)
-        archive.m_context.process_updated_raw_file(file_name)
+    if overwrite or not archive.m_context.raw_path_exists(file_name):
+        with archive.m_context.update_entry(
+            file_name,
+            write=True,
+            process=True
+        ) as entry:
+            entry['data'] = entity.m_to_dict(with_root_def=True)
     return get_reference(
         archive.metadata.upload_id, get_entry_id_from_file_name(file_name, archive)
     )
